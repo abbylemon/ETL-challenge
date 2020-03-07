46a00fa0a9d94c751480e9515835cf70eadf28c1
physics_nerd <abby.lemon22@gmail.com>
2020-03-07 10:45:23 -0600

    added my sql code to the common file

--- ETL-challenge.sql
+++ ETL-challenge.sql
@@ -10,3 +10,32 @@ CREATE TABLE starbucks_locations (
 select * from starbucks_locations;
 
 	
+create table target_locations (
+	target_store_id int primary key,
+	street_address text,
+	city text,
+	state_abr text,
+	striped_zip int
+);
+
+create table starbucks_menu(
+	beverage_category text,
+	beverage text,
+	beverage_prep text,
+	calories int,
+	total_Fat_g text,
+	trans_Fat_g decimal,
+	saturated_Fat_g decimal,
+	sodium_mg int,
+	total_Carbohydrates_g int,
+	cholesterol_mg int,
+	dietary_Fibre_g int,
+	sugars_g int,
+	protein_g decimal,
+	vitamin_A_DV text,
+	vitamin_C_DV text,
+	calcium_DV text,
+	iron_DV text,
+	caffeine_mg text
+);
+
